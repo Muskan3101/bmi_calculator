06ba8b4f5033ef04953d685e5cd549f3a913115e
Muskan3101 <muskanguptamuk1999@gmail.com>
2022-05-07 15:16:04 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/homescreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'BMI Calculator',
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
---
+++ lib/screens/homescreen.dart
@@ -0,0 +1,155 @@
+import 'package:bmi_calculator/constants/app_constants.dart';
+import 'package:bmi_calculator/widgets/left_bar.dart';
+import 'package:bmi_calculator/widgets/right_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  TextEditingController _heightController = TextEditingController();
+  TextEditingController _weightController = TextEditingController();
+  double _bmiResult = 0;
+  String _textResult = "";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "BMI Calculator",
+          style: TextStyle(color: accentHexColor, fontWeight: FontWeight.w300),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      backgroundColor: mainHexColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Container(
+                  width: 130,
+                  child: TextField(
+                    controller: _heightController,
+                    style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.w300,
+                        color: accentHexColor),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Height(m)",
+                        hintStyle: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white.withOpacity(0.8))),
+                  ),
+                ),
+                Container(
+                  width: 130,
+                  child: TextField(
+                    controller: _weightController,
+                    style: TextStyle(
+                        fontSize: 42,
+                        fontWeight: FontWeight.w300,
+                        color: accentHexColor),
+                    keyboardType: TextInputType.number,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Weight(kg)",
+                        hintStyle: TextStyle(
+                            fontSize: 26,
+                            fontWeight: FontWeight.w300,
+                            color: Colors.white.withOpacity(0.8))),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            GestureDetector(
+              onTap: _bmiCalc,
+              child: Container(
+                child: Text(
+                  "Calculate",
+                  style: TextStyle(fontSize: 22, color: accentHexColor),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            Container(
+              child: Text(
+                _bmiResult.toStringAsFixed(2),
+                style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.w400,
+                    color: accentHexColor),
+              ),
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Visibility(
+                visible: _textResult.isNotEmpty,
+                child: Text(
+                  _textResult,
+                  style: TextStyle(
+                      fontSize: 32,
+                      fontWeight: FontWeight.w400,
+                      color: accentHexColor),
+                )),
+            const SizedBox(
+              height: 10,
+            ),
+            const LeftBar(barWidth: 40),
+            const SizedBox(
+              height: 20,
+            ),
+            const LeftBar(barWidth: 70),
+            const SizedBox(
+              height: 20,
+            ),
+            const LeftBar(barWidth: 40),
+            const SizedBox(
+              height: 20,
+            ),
+            const RightBar(barWidth: 70),
+            const SizedBox(
+              height: 50,
+            ),
+            const RightBar(barWidth: 70),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _bmiCalc() {
+    double _height = double.parse(_heightController.text);
+    double _weight = double.parse(_weightController.text);
+    setState(() {
+      _bmiResult = _weight / (_height * _height);
+      if (_bmiResult > 25) {
+        _textResult = "You're Over Weight";
+      } else if (_bmiResult >= 18.5 && _bmiResult <= 25) {
+        _textResult = "You have Normal Weight";
+      } else {
+        _textResult = "You're Under Weight";
+      }
+    });
+  }
+}
